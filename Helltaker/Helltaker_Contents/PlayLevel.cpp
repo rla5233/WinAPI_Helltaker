4ecb7966445e9f554387d731030211f02306bf95
rla5233 <rla5233@naver.com>
2024-01-29 16:36:15 +0900

    Resources 폴더 생성

--- Helltaker/Helltaker_Contents/PlayLevel.cpp
+++ Helltaker/Helltaker_Contents/PlayLevel.cpp
@@ -2,6 +2,8 @@
 #include "Player.h"
 #include "Monster.h"
 
+#include <EngineBase/EnginePath.h>
+
 UPlayLevel::UPlayLevel()
 {}
 
@@ -11,6 +13,10 @@ UPlayLevel::~UPlayLevel()
 
 void UPlayLevel::BeginPlay()
 {
+	UEnginePath ResourcesPath = UEnginePath();
+	ResourcesPath.MoveParent();
+	ResourcesPath.Move("Resources");
+
 	SpawnActor<Player>();
 	SpawnActor<Monster>();
 }
