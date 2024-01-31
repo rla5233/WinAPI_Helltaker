a033b8dd88bca667970edf8f4ca78b10303ae563
rla5233 <rla5233@naver.com>
2024-02-01 01:10:02 +0900

    주석 수정

--- Helltaker/Helltaker_Contents/Hero.cpp
+++ Helltaker/Helltaker_Contents/Hero.cpp
@@ -16,11 +16,12 @@ void Hero::BeginPlay()
 	Renderer = CreateImageRenderer(0);
 	Renderer->SetImage("Hero0022.bmp");
 	
-	// 앞의 좌표 Actor와의 상대위치 {0, 0} = Actor 위치
-	// 뒤의 크기로 변환
+	// 앞의 좌표 Actor와의 상대위치
+	// {0, 0} => Actor 위치가 렌더 이미지 중심 위치
+	// 뒤의 크기로 렌더 이미지 크기 변환
 	Renderer->SetTransform({ {0, 0}, {70, 70} });
 
-	// 앞의 좌표부터 뒤의 좌표까지 이미지를 자른다.
+	// Set 이미지의 앞의 좌표부터 뒤의 크기만큼 이미지를 자른다.
 	// 이미지 크기 벗어나면 인식을 못함
 	Renderer->SetImageCuttingTransform({ {0, 0}, {89, 92} });
 	
