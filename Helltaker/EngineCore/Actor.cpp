593c0a936da0b6d8807e1f31ab8d068f70df3c14
rla5233 <rla5233@naver.com>
2024-02-16 10:10:20 +0900

    Engine : Level2 업데이트

--- Helltaker/EngineCore/Actor.cpp
+++ Helltaker/EngineCore/Actor.cpp
@@ -72,6 +72,12 @@ void AActor::Destroy(float _DestroyTime /*= 0.0f*/)
 	{
 		Renderer->Destroy(_DestroyTime);
 	}
+
+	for (UCollision* Collision : Collisions)
+	{
+		Collision->Destroy(_DestroyTime);
+	}
+
 }
 
 void AActor::DestroyUpdate(float _DeltaTime)
@@ -83,6 +89,11 @@ void AActor::DestroyUpdate(float _DeltaTime)
 	{
 		Renderer->DestroyUpdate(_DeltaTime);
 	}
+
+	for (UCollision* Collision : Collisions)
+	{
+		Collision->DestroyUpdate(_DeltaTime);
+	}
 }
 
 void AActor::ActiveUpdate(float _DeltaTime)
@@ -93,5 +104,10 @@ void AActor::ActiveUpdate(float _DeltaTime)
 	for (UImageRenderer* Renderer : Renderers)
 	{
 		Renderer->ActiveUpdate(_DeltaTime);
+	}
+
+	for (UCollision* Collision : Collisions)
+	{
+		Collision->ActiveUpdate(_DeltaTime);
 	}
 }
