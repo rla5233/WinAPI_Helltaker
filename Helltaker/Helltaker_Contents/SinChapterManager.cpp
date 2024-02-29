f899a9cc43a2863f61a06d180cb06cf44d23d4e7
rla5233 <rla5233@naver.com>
2024-03-01 01:44:59 +0900

    SinChapterManager : SinPit 추가

--- Helltaker/Helltaker_Contents/SinChapterManager.cpp
+++ Helltaker/Helltaker_Contents/SinChapterManager.cpp
@@ -5,8 +5,8 @@
 
 bool SinChapterManager::IsLoad = false;
 
-const int SinChapterManager::SinPitCount = 1;
-const FVector SinChapterManager::SinPitInterval = { 0.0f, 0.6074f };
+const int SinChapterManager::SinPitCount = 3;
+const FVector SinChapterManager::SinPitInterval = { 0.0f, 0.6079f };
 
 SinChapterManager::SinChapterManager()
 {
@@ -34,28 +34,31 @@ void SinChapterManager::M_CreateSinBG(std::string_view _Name)
 {
 	BackGround* SinBG = SpawnActor<BackGround>(static_cast<int>(SinUpdateOrder::BackGround));
 	SinBG->CreateBackGround(_Name, EBackGroundType::Sin);
-	SinBG->GetImageRenderer()->SetAlpha(0.8f);
+	SinBG->GetImageRenderer()->SetAlpha(0.75f);
 
 	AllSMapActors[reinterpret_cast<__int64>(SinBG)] = SinBG;
 }
 
 void SinChapterManager::M_CreateSinPit()
 {
+	FVector WinScale = ContentsHelper::GetWindowScale();
+	FVector Scale = { WinScale.X * 0.283f, WinScale.Y * 0.607f };
+	FVector Pos = { WinScale.X * 0.359f, WinScale.Y * 0.196f };
+
 	for (int i = 0; i < SinPitCount; i++)
-	{
-		FVector WinScale = ContentsHelper::GetWindowScale();
-		FVector Scale = { WinScale.X * 0.283f, WinScale.Y * 0.607f };
-		FVector Pos = { WinScale.X * 0.359f, WinScale.Y * 0.196f };
-		
+	{		
 		SinPit.push_back(SpawnActor<SinComponent>(static_cast<int>(SinUpdateOrder::BackGround)));
-		SinPit[i]->SetActorLocation({ WinScale.hX() - Pos.X, Pos.Y });
+		SinPit[i]->SetActorLocation({ WinScale.hX(), Pos.Y  });
+
 		SinPit[i]->CreateImageRenderer("Left_Pit", SinRenderOrder::UnderBackGround);
 		SinPit[i]->GetImageRenderer("Left_Pit")->SetImage("Sin_Pit_Left.png");
-		SinPit[i]->GetImageRenderer("Left_Pit")->SetTransform({ {0, 0}, Scale });
-		
+		SinPit[i]->GetImageRenderer("Left_Pit")->SetTransform({ { -Pos.X, 0.0f }, Scale });
 
-		//SinPit[i]->CreateImageRenderer("Right_Pit", SinRenderOrder::UnderBackGround);
-		//SinPit[i]->GetImageRenderer("Right_Pit")->SetImage("Sin_Pit_Right.png");
+		SinPit[i]->CreateImageRenderer("Right_Pit", SinRenderOrder::UnderBackGround);
+		SinPit[i]->GetImageRenderer("Right_Pit")->SetImage("Sin_Pit_Right.png");
+		SinPit[i]->GetImageRenderer("Right_Pit")->SetTransform({ { Pos.X, 0.0f }, Scale });
+
+		Pos += WinScale * SinPitInterval;
 	}
 }
 
