64152a25e616353b11b8a4bd6ea54b3c7fc60018
rla5233 <rla5233@naver.com>
2024-03-03 17:55:56 +0900

    Sin : Chain 리팩토링

--- Helltaker/Helltaker_Contents/Chain.cpp
+++ Helltaker/Helltaker_Contents/Chain.cpp
@@ -32,12 +32,37 @@ void Chain::Move(float _DeltaTime)
 void Chain::Tick(float _DeltaTime)
 {
 	SinComponent::Tick(_DeltaTime);
+
+	StateUpdate(_DeltaTime);
 }
 
 void Chain::StateUpdate(float _DeltaTime)
 {
+	switch (State)
+	{
+	case ESinChainState::Idle:
+		Idle(_DeltaTime);
+		break;
+	case ESinChainState::Move:
+		Move(_DeltaTime);
+		break;
+	}
 }
 
 void Chain::StateChange(ESinChainState _State)
 {
+	if (State != _State)
+	{	
+		switch (_State)
+		{
+		case ESinChainState::Idle:
+			IdleStart();
+			break;
+		case ESinChainState::Move:
+			MoveStart();
+			break;
+		}
+	}
+
+	State = _State;
 }
